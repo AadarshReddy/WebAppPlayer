52a71a31a514c311cfa0d85e2f6da1f6e5124a00
Aadarsh Reddy <areddy5757@gmail.com>
2024-02-05 14:24:12 +0530

    Add project files.

---
+++ WebAppPlayer/Controllers/PlayerController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebAppPlayer.Models;
+
+namespace WebAppPlayer.Controllers
+{
+    public class PlayerController : Controller
+    {
+        static List<Player> listplay = new List<Player>()
+        {
+            new Player{Id=1,Name="Sam",Description="Manager"},
+            new Player{Id=2,Name="Ritu",Description="Hr"}
+        };
+        public IActionResult Index()
+        {
+            return View(listplay);
+        }
+    }
+}
